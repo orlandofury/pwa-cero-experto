8ddb1e2b802b935e0f7afc2ccceeb6b826273a87
Orlando Flores Silva <orlandofury@gmail.com>
2023-03-28 23:06:18 -0600

    Final seccion 4

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,6 +1,10 @@
+var self = this;
 self.addEventListener('fetch',event => {
-    if(event.request.url.includes('main.jpg')){
-        let fotoReq = fetch('img/main-patas-arriba.jpg');
-        event.respondWith(fotoReq);
-    }
+    const resp = fetch(event.request)
+    .then(res => {
+        //Los errores 404 y 400 no se devuelven en el catch
+        //se deben manejar internamente en el then
+        return res.ok ? res:fetch('img/main.jpg');
+    });
+    event.respondWith(resp);
 })
