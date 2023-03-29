fbb999cf8f544e6c3e3180c80f55ba2d3555a1a8
Orlando Flores Silva <orlandofury@gmail.com>
2023-03-28 22:36:39 -0600

    Tarea de seccion 4

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,23 +1,6 @@
 self.addEventListener('fetch',event => {
-    if(event.request.url.includes('style.css')){
-        let respuesta = new Response(`
-            body{
-                background-color: red !important;
-                color:pink;
-            }
-        `,{
-            headers:{
-                'Content-Type':'text/css'
-            }
-        });
-        event.respondWith(respuesta);
+    if(event.request.url.includes('main.jpg')){
+        let fotoReq = fetch('img/main-patas-arriba.jpg');
+        event.respondWith(fotoReq);
     }
-    // if(event.request.url.includes('.jpg')){
-    //     console.log(event.request.url);
-
-    //     //let fotoReq = fetch('img/main.jpg');
-    //     //let fotoReq = fetch('event.request.url');
-    //     let fotoReq = fetch(event.request);
-    //     event.respondWith(fotoReq);
-    // }
 })
