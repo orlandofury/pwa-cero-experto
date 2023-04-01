90729b65c1c0f7575e0b1ab23f3d7e3e76510121
Orlando Flores Silva <orlandofury@gmail.com>
2023-03-31 22:17:40 -0600

    Ahora si codigo inicial de la tarea

---
+++ 09-pouchdb-manual/app.js
@@ -0,0 +1,42 @@
+
+
+// Entrenamiento PouchDB
+
+// 1- Crear la base de datos
+// Nombre:  mensajes
+
+
+
+// Objeto a grabar en base de datos
+let mensaje = {
+    _id: new Date().toISOString(),
+    user: 'spiderman',
+    mensaje: 'Mi tía hizo unos panqueques muy buenos',
+    sincronizado: false
+};
+
+
+// 2- Insertar en la base de datos
+
+
+
+// 3- Leer todos los mensajes offline
+
+
+
+
+// 4- Cambiar el valor 'sincronizado' de todos los objetos
+//  en la BD a TRUE
+
+
+
+
+// 5- Borrar todos los registros, uno por uno, evaluando
+// cuales estan sincronizados
+// deberá de comentar todo el código que actualiza
+// el campo de la sincronización 
+
+
+
+
+
