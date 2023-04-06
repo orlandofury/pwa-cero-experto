40df4e26b34ca11df1242eb0a8486048f42b5788
Orlando Flores Silva <orlandofury@gmail.com>
2023-04-06 10:32:01 -0600

    Enviando la peticion POST desde el boton

---
+++ 10-twittor-offline-posting/server/routes.js
@@ -0,0 +1,39 @@
+// Routes.js - Módulo de rutas
+var express = require('express');
+var router = express.Router();
+
+
+const mensajes = [
+  {
+    _id : 'xxx',
+    user: 'spiderman',
+    mensaje: 'Hola Mundo'
+  }
+]
+
+
+
+
+// Get mensajes
+router.get('/', function (req, res) {
+  //res.json('Obteniendo mensajes');
+  res.json(mensajes);
+});
+
+router.post('/', function (req, res) {
+  
+  const mensaje = {
+    mensaje: req.body.mensaje,
+    user: req.body.user
+  }
+  mensajes.push(mensaje);
+
+  res.json({
+    ok:true,
+    mensaje
+  })
+});
+
+
+
+module.exports = router;
